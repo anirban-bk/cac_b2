061f3373388a39b2ca97360ef243159c7c7ca14b
anirbanb <Anirbanb@eidesign.net>
2024-05-01 15:19:53 +0530

    commit get github api in index.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.get('/login', (req, res)=>{
     res.send('<h2>this is an attempt to login at cacb2</h2>');
 });
 app.get('/twitter', (req, res)=>{res.send('twitter response from server')});
+app.get('/github', (req, res)=>res.json(githubData));
 app.listen(port, ()=>{
     //console.log(process.env);
     console.log(`app is listening on port ${port}`)
